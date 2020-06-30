d2764cd433d5b3d9f4aa2f75ea627158cf766fd7
Mathias Bjerke <mathbje@gmail.com>
2020-06-30 12:47:54 +0200

    Added norwegian translations

---
+++ plugin/edipost/translations/no.php
@@ -0,0 +1,20 @@
+<?php
+
+global $_MODULE;
+$_MODULE = array();
+$_MODULE['<{edipost}prestashop>edipost_8044e1c3222ab802d9c30b6732100728'] = 'Edipost';
+$_MODULE['<{edipost}prestashop>edipost_f4f70727dc34561dfde1a3c529b6205c'] = 'Innstillinger';
+$_MODULE['<{edipost}prestashop>edipost_30dbd3dffa9dbd08b062436ba8dcbad7'] = 'Produksjon';
+$_MODULE['<{edipost}prestashop>edipost_c3005e926c980fe463fcc222fdfb3ddd'] = 'Utvikling- eller produksjonsmiljø';
+$_MODULE['<{edipost}prestashop>edipost_756d97bb256b8580d4d71ee0c547804e'] = 'Produksjonmilje';
+$_MODULE['<{edipost}prestashop>edipost_330f49df8243756a8a4dc7f7f7ee6dfe'] = 'Utviklingsmiljø';
+$_MODULE['<{edipost}prestashop>edipost_cc10c7996e9cf55d9027ce3f51b9d31a'] = 'Legg inn en gyldig API-nøkkel';
+$_MODULE['<{edipost}prestashop>edipost_656a6828d7ef1bb791e42087c4b5ee6e'] = 'API-nøkkel';
+$_MODULE['<{edipost}prestashop>edipost_f6039d44b29456b20f8f373155ae4973'] = 'Brukernavn';
+$_MODULE['<{edipost}prestashop>edipost_dc647eb65e6711e155375218212b3964'] = 'Passord';
+$_MODULE['<{edipost}prestashop>edipost_c9cc8cce247e49bae79f15173ce97354'] = 'Lagre';
+$_MODULE['<{edipost}prestashop>edipost_96bb3c0e17c14ab0ab4fb116efc21027'] = '-- Velg en sendingsmetode --';
+$_MODULE['<{edipost}prestashop>edipost_78945de8de090e90045d299651a68a9b'] = 'Tilgjengelig';
+$_MODULE['<{edipost}prestashop>edipost_bce14e214d93c2d8ae968988d004f689'] = 'Ingen sendignsmåter tilgjengelig.';
+$_MODULE['<{edipost}prestashop>adminedipostcontroller_a095586e10687458b447bf9b8dafcfea'] = 'Feil parametre';
+
